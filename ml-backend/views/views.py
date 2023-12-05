2554147328076ed76b261cd9ffd04aa7cd6fb09c
MUKILAN S <mukilan069@gmail.com>
2023-12-06 01:46:51 +0530

    1 Week Data prediction API route

---
+++ ml-backend/views/views.py
@@ -0,0 +1,17 @@
+from flask import jsonify, request
+import numpy as np
+import tensorflow as tf
+
+from app import *
+
+weekmodel = tf.keras.models.load_model("models/weekPrediction.h5", compile=False)
+
+# Input 7 day emission
+# Output next 7 day emission
+@app.route('/week', methods = ['GET', 'POST'])
+def week():
+    print(request.json['data'])
+    data = request.json['data']
+    test = np.array(data).astype(np.float32)
+    pred = weekmodel.predict(test.reshape(1,7))
+    return jsonify({'data': pred.tolist()})
