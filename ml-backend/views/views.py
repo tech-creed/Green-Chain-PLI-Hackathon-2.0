d35223c8023ea8c6d30d05e24fcbc9e7b07259c0
MUKILAN S <mukilan069@gmail.com>
2023-12-06 01:49:29 +0530

    1 Month Data prediction API route

--- ml-backend/views/views.py
+++ ml-backend/views/views.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 from app import *
 
 weekmodel = tf.keras.models.load_model("models/weekPrediction.h5", compile=False)
+monthmodel = tf.keras.models.load_model("models/monthModel.h5", compile=False)
 
 # Input 7 day emission
 # Output next 7 day emission
@@ -14,4 +15,13 @@ def week():
     data = request.json['data']
     test = np.array(data).astype(np.float32)
     pred = weekmodel.predict(test.reshape(1,7))
+    return jsonify({'data': pred.tolist()})
+
+# Input 30 day emission
+# Output next 30 day emission
+@app.route('/month', methods = ['GET', 'POST'])
+def month():
+    data = request.json['data']
+    test = np.array(data).astype(np.float32)
+    pred = monthmodel.predict(test.reshape(1,30))
     return jsonify({'data': pred.tolist()})
